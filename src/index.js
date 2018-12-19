9ef766c17c092694f7a0c3e228fb0b0dc9925717
Aitor <aitor@rojo2.com>
2018-12-19 12:54:51 +0100

    Add Access-Control-Allow-Credentials

--- src/index.js
+++ src/index.js
@@ -25,7 +25,9 @@ import pkg from '../package.json'
 
 const app = new Koa()
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  credentials: true
+}))
 app.use(compress())
 app.use(bodyParser())
 app.use(async (ctx, next) => {
